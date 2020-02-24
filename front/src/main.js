0dad8835667d203b5076bd8494872ab266923688
Klara <sanchez.clara13@gmail.com>
2020-02-24 21:09:06 +0100

    added dependency

--- front/src/main.js
+++ front/src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
