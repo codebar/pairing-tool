96a1fb4f991e9f53d295aef8e6f283c9982422aa
Klara <sanchez.clara13@gmail.com>
2020-02-24 22:04:06 +0100

    Form and List created

--- front/src/components/StudentList.vue
+++ front/src/components/StudentList.vue
@@ -1,55 +1,62 @@
 <template>
-  <div id="student-list">
-    <h1>Student List</h1>
-    <div id="list">
-      <ul>
-        <li>
-          <p>Name: {{ this.userData.fName }} {{ userData.lName }}</p>
-          <p>Email: {{ userData.email }}</p>
-        </li>
-      </ul>
+  <div id="workshop" class="row">
+    <div class="col-12">
+      <h1>Codebar's Super Great and Awesome: Pairing Tool!!!</h1>
+      <hr />
+      <form>
+        <div class="form-group col-4">
+          <h3>Student's Form</h3>
+          <label for="fname">First Name</label>
+          <input
+            type="text"
+            id="fname"
+            class="form-control"
+            aria-describedby="first-name"
+            placeholder="First Name"
+            v-model="userData.fName"
+          />
+          <label for="email">Last Name</label>
+          <input
+            type="text"
+            id="lname"
+            class="form-control"
+            aria-describedby="last-name"
+            placeholder="Last Name"
+            v-model="userData.lName"
+          />
+          <label for="email">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            :value="userData.email"
+            @input="userData.email = $event.target.value"
+          />
+          <div class="m-2">
+            <button
+              id="add-student"
+              class="btn btn-primary"
+              @click.prevent="submitted"
+            >
+              + Add Student!
+            </button>
+          </div>
+        </div>
+      </form>
+      <div id="student-list" class="col-4">
+        <h2>Student List</h2>
+        <ul>
+          <li>
+            <p>
+              Name: {{ userData.fName }} {{ userData.lName }} Email:
+              {{ userData.email }}
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
-    <form>
-      <div class="form-group">
-        <label for="fname">First Name</label>
-        <input
-          type="text"
-          id="fname"
-          class="form-control"
-          aria-describedby="first-name"
-          placeholder="First Name"
-          v-model="userData.fName"
-        />
-        <label for="email">Last Name</label>
-        <input
-          type="text"
-          id="lname"
-          class="form-control"
-          aria-describedby="last-name"
-          placeholder="Last Name"
-          v-model="userData.lName"
-        />
-        <label for="email">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-          :value="userData.email"
-          @input="userData.email = $event.target.value"
-        />
-      </div>
-      <div>
-        <button
-          id="add-student"
-          class="btn btn-primary"
-          @click.prevent="submitted"
-        >
-          + Add Student!
-        </button>
-      </div>
-    </form>
   </div>
 </template>
 
