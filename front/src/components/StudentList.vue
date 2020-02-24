61811be9a40e686fc2ebaf5177af1aacba858c7a
Klara <sanchez.clara13@gmail.com>
2020-02-24 20:58:28 +0100

    created studentList component

---
+++ front/src/components/StudentList.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="student-list">
+    <h1>student List </h1>
+			<form>
+          <div class="form-group">
+            <label for="email">Mail</label>
+            <input
+              type="text"
+              id="email"
+              class="form-control"
+              :value="userData.email"
+              @input="userData.email = $event.target.value"
+            />
+          </div>
+            <div>
+
+          <button class="btn btn-primary" @click.prevent="submitted">
+            Submit!
+          </button>
+            </div>
+    
+      </form>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <StudentList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import StudentList from "@/components/StudentList.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    StudentList
   }
 };
 </script>
