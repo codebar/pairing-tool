6af33a78bf70e11cffb751d90ca1baa2c3d40df0
Klara <sanchez.clara13@gmail.com>
2020-02-24 21:24:25 +0100

    deleted Logo

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <StudentList>
+    <StudentList />
   </div>
 </template>
 
