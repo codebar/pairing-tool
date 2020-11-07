ce422b4dbcc7975d5d37d2ae1a78fc2c5897798d
saljuama <saljuama@gmail.com>
2020-11-07 05:15:01 +0100

    polish the modal for attendees

--- src/app/features/workshopAttendees/WorkshopAttendees.js
+++ src/app/features/workshopAttendees/WorkshopAttendees.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import Button from '@material-ui/core/Button'
-import './WorkshopAttendees.scss'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
@@ -11,6 +10,8 @@ import {AccountCircle, Create} from '@material-ui/icons'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
 import Rating from '@material-ui/lab/Rating'
+import Box from '@material-ui/core/Box'
+import './WorkshopAttendees.scss'
 
 export const WorkshopAttendees = () => (
   <div className='WorkshopAttendees'>
@@ -29,7 +30,6 @@ export const WorkshopAttendees = () => (
 )
 
 
-
 const AddParticipantModal = ({role}) => {
   const [open, setOpen] = useState(false)
   const openModal = () => setOpen(true)
@@ -41,20 +41,15 @@ const AddParticipantModal = ({role}) => {
       <Dialog open={open} onClose={closeModal} aria-labelledby={`add-${role}-form-dialog-title`}>
         <DialogTitle id={`add-${role}-form-dialog-title`}>{`Add new ${role}`}</DialogTitle>
         <DialogContent className='omg'>
-          <Grid container spacing={1} alignItems='flex-end'>
-            <Grid item xs={1}><AccountCircle /></Grid>
-            <Grid item xs={10}><TextField id='input-with-icon-grid' label='Name (pronouns)' fullWidth/></Grid>
-          </Grid>
-          <Grid container spacing={1} alignItems='flex-end'>
-            <Grid item xs={1}><Create /></Grid>
-            <Grid item xs={10}><TextField id='input-with-icon-grid' label='Skills' multiline rowsMax={3} fullWidth/></Grid>
-          </Grid>
-          <Skill name='HTML'/>
-          <Skill name='CSS'/>
-          <Skill name='Javascript'/>
-          <Skill name='Python'/>
-          <Skill name='SQL'/>
-          <Skill name='Java'/>
+          <Input
+            icon={<AccountCircle/>}
+            control={<TextField id='input-with-icon-grid' label='Name (pronouns)' fullWidth/>}
+          />
+          <Input
+            icon={<Create/>}
+            control={<TextField id='input-with-icon-grid' label='Notes' multiline rowsMax={3} fullWidth/>}
+          />
+          <Skills/>
         </DialogContent>
         <DialogActions>
           <Button onClick={closeModal} color='secondary'>CANCEL</Button>
@@ -65,31 +60,72 @@ const AddParticipantModal = ({role}) => {
   )
 }
 
+const Input = ({icon, control}) => {
+  return (
+    <Grid container spacing={1} alignItems='flex-end'>
+      <Grid item xs={1}>{icon}</Grid>
+      <Grid item xs={10}>{control}</Grid>
+    </Grid>
+  )
+}
+
+const Skills = () => {
+  const skills = ['HTML', 'CSS', 'Javascript', 'Python', 'SQL', 'Java']
+
+  return (
+    <div>
+      <Grid container spacing={1} alignItems='center'>
+        <Grid item xs={1}/>
+        <Grid item xs={10}>
+          <h4>Skills</h4>
+          {skills.map(skill => (<Skill name={skill}/>))}
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
 const Skill = ({name}) => {
   const [checked, setChecked] = useState(false)
   const [value, setValue] = useState(0)
+  const [hover, setHover] = useState(-1)
 
-  return (<Grid container spacing={1} alignItems='flex-end'>
-    <Grid item xs={3}/>
-    <Grid item xs={3}>
-      <FormControlLabel
-        control={
-          <Checkbox checked={checked} onChange={event => {
-            setChecked(event.target.checked)
-            setValue(0)
-          }} name={name} color='primary'/>
-        }
-        label={name}
-      />
-    </Grid>
-    <Grid item xs={3}>
-      <Rating
-        className={'Rating'}
-        name={`${name}-rating`}
-        value={value}
-        onChange={(event, newValue) => setValue(newValue)}
-        disabled={!checked}
-      />
-    </Grid>
-  </Grid>)
+  const labels = {
+    null: 'N/A',
+    0: 'N/A',
+    1: 'Inexperienced',
+    2: 'Basic',
+    3: 'Intermediate',
+    4: 'Advanced',
+    5: 'Expert'
+  }
+
+  const toggleSkill = event => {
+    setChecked(event.target.checked)
+    setValue(0)
+  }
+
+  return (
+    <Grid container className={'Skill'} spacing={1} alignItems='flex-end'>
+      <Grid item xs={4}>
+        <FormControlLabel
+          control={<Checkbox checked={checked} onChange={toggleSkill} name={name} color='primary'/>}
+          label={name}
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <Rating
+          className={'SkillRating'}
+          name={`${name}-rating`}
+          value={value}
+          onChange={(event, newValue) => setValue(newValue)}
+          onChangeActive={(event, newHover) => setHover(newHover)}
+          disabled={!checked}
+        />
+      </Grid>
+      <Grid item xs={4}>
+        {!!checked && value !== null &&
+        <Box className={'SkillLegend'} ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
+      </Grid>
+    </Grid>)
 }
