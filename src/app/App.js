4adb656b9aa2a0ca1d3e47a4de283454be69d4d3
saljuama <saljuama@gmail.com>
2020-11-13 18:50:25 +0100

    repurpose App to maintain the main areas, header, content, etc

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import {WorkshopAttendees} from './features/workshopAttendees/WorkshopAttendees'
+import {Attendees} from './features/attendees/Attendees'
+import smallLogo from './logo200.png'
 import './App.scss'
 
 const App = () => (
   <div className='App'>
-    <WorkshopAttendees/>
+    <div className='AppHeader'>
+      <img alt='Codebar' src={smallLogo} />
+    </div>
+    <div className='AppContent'>
+      <Attendees/>
+    </div>
+    <div className='AppFooter'>
+      <p>Codebar - Pairing Tool</p>
+    </div>
   </div>
 )
 
