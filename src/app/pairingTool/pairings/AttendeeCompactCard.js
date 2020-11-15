0bff6142f08d430856b3f249de2d7601bb12967e
saljuama <saljuama@gmail.com>
2020-11-15 04:59:19 +0100

    can't change languages in the pairings step

--- src/app/pairingTool/pairings/AttendeeCompactCard.js
+++ src/app/pairingTool/pairings/AttendeeCompactCard.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import {useSelector} from 'react-redux'
 import {selectLanguages} from '../../configuration/configurationSlice'
-import {toggleLanguage} from '../attendees/attendeesSlice'
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -9,7 +8,6 @@ import './AttendeeCompactCard.scss'
 
 export const AttendeeCompactCard = ({data}) => {
   const languages = useSelector(selectLanguages)
-  const dispatch = useDispatch()
   return (
     <Card className='AttendeeCompactCard'>
       <CardContent className='CardContent'>
@@ -20,7 +18,6 @@ export const AttendeeCompactCard = ({data}) => {
               className={`${language}Button ${data.languages.includes(language) ? 'Active' : 'Inactive'}`}
               variant='contained'
               color='primary'
-              onClick={() => dispatch(toggleLanguage({id: data.id, language}))}
             >
               {language}
             </Button>
