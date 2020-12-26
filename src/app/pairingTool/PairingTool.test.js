217bff359c08ac8f139e5e82ad83ca34d22bc6b6
saljuama <saljuama@gmail.com>
2020-12-26 17:55:19 +0100

    missing refactor rename

--- src/app/pairingTool/PairingTool.test.js
+++ src/app/pairingTool/PairingTool.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {PairingTool} from './PairingTool'
-import {render} from '../../test/testUtils'
+import {renderComponent} from '../../test/testUtils'
 
 describe('The Workshop Attendees component', () => {
 
   it('renders a title and a dropzone', () => {
-    const {getByText} = render(<PairingTool/>)
+    const {getByText} = renderComponent(<PairingTool/>)
 
     expect(getByText(/Pairing Tool/i)).toBeInTheDocument()
     expect(getByText(/Drag and Drop/i)).toBeInTheDocument()
