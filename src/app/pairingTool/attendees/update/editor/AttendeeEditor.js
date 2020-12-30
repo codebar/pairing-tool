b6a12a9abf0c502f278732d042cbe225fcbf0477
saljuama <saljuama@gmail.com>
2020-12-30 23:11:55 +0100

    add notes, skills and tutorial inputs and some css

--- src/app/pairingTool/attendees/update/editor/AttendeeEditor.js
+++ src/app/pairingTool/attendees/update/editor/AttendeeEditor.js
@@ -11,21 +11,71 @@ import './AttendeeEditor.scss'
 export const AttendeeEditor = ({attendee}) => {
 
   const attendeeNameInput =
-    <TextField label='Name' value={attendee.name} onChange={() => {}} data-test-id='attendee-edit-name-input'/>
+    <TextField
+      data-test-id='attendee-edit-name-input'
+      label='Name'
+      value={attendee.name}
+      onChange={() => {}}
+    />
 
   const attendeeRoleRadioButtons =
     <FormControl component='fieldset'>
-      <FormLabel component='legend'>Role</FormLabel>
-      <RadioGroup row aria-label='role' name='role' value={attendee.role} onChange={() => {}}>
-        <FormControlLabel value='Student' control={<Radio data-test-id={'attendee-edit-role-student'}/>} label='Student'/>
-        <FormControlLabel value='Coach' control={<Radio data-test-id={'attendee-edit-role-coach'}/>} label='Coach'/>
+      <FormLabel component='label'>Role</FormLabel>
+      <RadioGroup
+        row aria-label='role' name='role'
+        value={attendee.role}
+        onChange={() => {}}
+      >
+        <FormControlLabel
+          label='Student'
+          value='Student'
+          control={<Radio data-test-id={'attendee-edit-role-student'}/>}
+        />
+        <FormControlLabel
+          label='Coach'
+          value='Coach'
+          control={<Radio data-test-id={'attendee-edit-role-coach'}/>}
+        />
       </RadioGroup>
     </FormControl>
+
+  const attendeeNotesInput =
+    <TextField
+      data-test-id='attendee-edit-notes-input'
+      label='Notes'
+      value={attendee.notes}
+      onChange={() => {}}
+    />
+
+  const skillsInput =
+    <TextField
+      data-test-id='attendee-edit-skills-input'
+      label='Skills'
+      multiline
+      rowsMax={2}
+      value={attendee.skills}
+      onChange={() => {}}
+      disabled={attendee.role === 'Student'}
+    />
+
+  const tutorialInput =
+    <TextField
+      data-test-id='attendee-edit-tutorial-input'
+      label='Tutorial'
+      multiline
+      rowsMax={2}
+      value={attendee.tutorial}
+      onChange={() => {}}
+      disabled={attendee.role === 'Coach'}
+    />
 
   return (
     <div className='AttendeeEditor' data-test-id='attendee-edit-form'>
       {attendeeNameInput}
       {attendeeRoleRadioButtons}
+      {attendeeNotesInput}
+      {skillsInput}
+      {tutorialInput}
     </div>
   )
 }
--- src/app/pairingTool/attendees/update/editor/AttendeeEditor.test.js
+++ src/app/pairingTool/attendees/update/editor/AttendeeEditor.test.js
@@ -1,29 +1,65 @@
 import {renderComponent, testStore} from '../../../../../test/testUtils'
-import {stateAfterParsingCsv, student} from '../../../../../test/fixtures/attendees'
+import {coach, stateAfterParsingCsv, student} from '../../../../../test/fixtures/attendees'
 import {overrideToggle} from '../../../../../config/togglesSlice'
 import {AttendeeEditor} from './AttendeeEditor'
 
 describe('The Attendee Editor', () => {
 
+
   const render = attendee => {
     const store = testStore(stateAfterParsingCsv)
     store.dispatch(overrideToggle({toggle: 'updateAttendeesNewScreen', value: 'true'}))
     const renderResult = renderComponent(<AttendeeEditor attendee={attendee} />, store)
+    const htmlInputFrom = container => container.getElementsByTagName('input')[0]
+    const htmlTextareaFrom = container => container.getElementsByTagName('textarea')[0]
     return {
       ...renderResult,
       store,
-      nameInput: () => renderResult.getByTestId('attendee-edit-name-input').getElementsByTagName('input')[0],
-      studentRadioInput: () => renderResult.getByTestId('attendee-edit-role-student').getElementsByTagName('input')[0],
-      coachRadioInput: () => renderResult.getByTestId('attendee-edit-role-coach').getElementsByTagName('input')[0],
+      nameInput: () => htmlInputFrom(renderResult.getByTestId('attendee-edit-name-input')),
+      studentRadioInput: () => htmlInputFrom(renderResult.getByTestId('attendee-edit-role-student')),
+      coachRadioInput: () => htmlInputFrom(renderResult.getByTestId('attendee-edit-role-coach')),
+      notesInput: () => htmlInputFrom(renderResult.getByTestId('attendee-edit-notes-input')),
+      skillsInput: () => htmlTextareaFrom(renderResult.getByTestId('attendee-edit-skills-input')),
+      tutorialInput: () => htmlTextareaFrom(renderResult.getByTestId('attendee-edit-tutorial-input')),
     }
   }
 
   it('renders a student with its existing information', () => {
-    const {nameInput, studentRadioInput, coachRadioInput} = render(student)
+    const {
+      nameInput,
+      studentRadioInput,
+      coachRadioInput,
+      notesInput,
+      skillsInput,
+      tutorialInput
+    } = render(student)
 
     expect(nameInput().value).toBe(student.name)
     expect(studentRadioInput()).toBeChecked()
+    expect(notesInput().value).toBe(student.notes)
+    expect(tutorialInput().value).toBe(student.tutorial)
+
     expect(coachRadioInput()).not.toBeChecked()
+    expect(skillsInput()).toBeDisabled()
+  })
+
+  it('renders a coach with its existing information', () => {
+    const {
+      nameInput,
+      studentRadioInput,
+      coachRadioInput,
+      notesInput,
+      skillsInput,
+      tutorialInput
+    } = render(coach)
+
+    expect(nameInput().value).toBe(coach.name)
+    expect(coachRadioInput()).toBeChecked()
+    expect(notesInput().value).toBe(coach.notes)
+    expect(skillsInput().value).toBe(coach.skills)
+
+    expect(studentRadioInput()).not.toBeChecked()
+    expect(tutorialInput()).toBeDisabled()
   })
 
 })
