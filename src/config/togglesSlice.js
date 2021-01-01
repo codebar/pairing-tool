75da92892591a4f6acd415590be83948c14170ab
saljuama <saljuama@gmail.com>
2021-01-01 23:46:57 +0100

    release new pairing screen :D

--- src/config/togglesSlice.js
+++ src/config/togglesSlice.js
@@ -1,7 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit'
 
 export const initialState = {
-  updateAttendeesNewScreen: false
+  updateAttendeesNewScreen: true
 }
 
 const togglesSlice = createSlice({
