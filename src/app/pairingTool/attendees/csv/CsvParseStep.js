a43e575ba1a552f3acd6effd2a3f9930b5d4b6c7
Salva Juan <saljuama@gmail.com>
2021-05-02 04:22:02 +0200

    clarify UI in step 1

--- src/app/pairingTool/attendees/csv/CsvParseStep.js
+++ src/app/pairingTool/attendees/csv/CsvParseStep.js
@@ -6,9 +6,9 @@ import './CsvParseStep.scss'
 export const CsvParseStep = () => {
   return (
     <div className='CsvParseStep'>
-      <h1>Pairing Tool</h1>
       <span>Step 1: Download the pairing CSV from the workshop page</span>
       <img alt='Pairing CSV' src={pairingCsvImg} />
+      <span>And click on this button to select and upload the CSV file</span>
       <UploadCsvButton/>
     </div>
   )
