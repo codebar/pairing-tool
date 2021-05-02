af510814eb1b84c2844ecb458a4e612fadea1eb9
Salva Juan <saljuama@gmail.com>
2021-05-02 22:59:31 +0200

    add detection rules to languages configuration

--- src/app/configuration/configurationSlice.js
+++ src/app/configuration/configurationSlice.js
@@ -2,15 +2,15 @@ import {createSlice} from '@reduxjs/toolkit'
 
 export const initialState = {
   languages: [
-    { name: 'HTML' },
-    { name: 'CSS' },
-    { name: 'JS' },
-    { name: 'Python' },
-    { name: 'Ruby' },
-    { name: 'SQL' },
-    { name: 'Java' },
-    { name: 'PHP' },
-    { name: 'Other' }
+    { name: 'HTML', alias: [], exclusions: [] },
+    { name: 'CSS', alias: [], exclusions: [] },
+    { name: 'JS', alias: ['javascript', 'typescript'], exclusions: [] },
+    { name: 'Python', alias: [], exclusions: [] },
+    { name: 'Ruby', alias: [], exclusions: [] },
+    { name: 'SQL', alias: [], exclusions: [] },
+    { name: 'Java', alias: [], exclusions: ['javascript'] },
+    { name: 'PHP', alias: [], exclusions: [] },
+    { name: 'Other', alias: [], exclusions: [] }
   ]
 }
 
@@ -24,4 +24,12 @@ const configurationSlice = createSlice({
 })
 export const configurationReducer = configurationSlice.reducer
 
-export const selectLanguageNames = state => state.configuration.languages.map(language => language.name)
+export const selectLanguageNames = state =>
+  state.configuration.languages.map(language => language.name)
+
+export const selectLanguageDetectionRules = state =>
+  state.configuration.languages.map(language => ({
+    name: language.name,
+    alias: language.alias,
+    exclusions: language.exclusions
+  }))
--- src/app/configuration/configurationSlice.test.js
+++ src/app/configuration/configurationSlice.test.js
@@ -1,11 +1,22 @@
-import {configurationReducer, initialState, selectLanguageNames} from './configurationSlice'
+import {
+  configurationReducer,
+  initialState,
+  selectLanguageDetectionRules,
+  selectLanguageNames
+} from './configurationSlice'
 
 describe('The Configuration Slice', () => {
 
   it('contains the list of languages to work with', () => {
     const nextState = configurationReducer(initialState, {})
-    expect(selectLanguageNames({configuration: nextState}))
-      .toEqual(['HTML', 'CSS', 'JS', 'Python', 'Ruby', 'SQL', 'Java', 'PHP', 'Other'])
+    const languageNames = selectLanguageNames({configuration: nextState})
+    expect(languageNames).toEqual(['HTML', 'CSS', 'JS', 'Python', 'Ruby', 'SQL', 'Java', 'PHP', 'Other'])
   })
-  
+
+  it('can see the language detection rules', () => {
+    const nextState = configurationReducer(initialState, {})
+    const languageDetectionRules = selectLanguageDetectionRules({configuration: nextState})
+    expect(languageDetectionRules).toContainEqual({name: 'Java', alias: [], exclusions:['javascript']})
+  })
+
 })
