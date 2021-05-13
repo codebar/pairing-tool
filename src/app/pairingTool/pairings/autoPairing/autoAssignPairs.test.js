2b403207d939a7006b7406bff2336c57cfee00fc
Salva Juan <saljuama@gmail.com>
2021-05-14 01:10:48 +0200

    refactor test more readable, easier to write ✨

--- src/app/pairingTool/pairings/autoPairing/autoAssignPairs.test.js
+++ src/app/pairingTool/pairings/autoPairing/autoAssignPairs.test.js
@@ -1,64 +1,56 @@
+import {testStore} from '../../../../test/testUtils'
 import {storeInitialState} from '../../../../config/store'
 import {autoAssignPairs} from './autoAssignPairs'
-import {moveCoachToGroup, moveStudentToGroup} from '../pairingsSlice'
+import {selectPairingGroups} from '../pairingsSlice'
 
-describe('Auto assign pairs thunk', () => {
+describe('Auto assign pairs', () => {
+
+  const runAutoPairingsFor = async availableParticipants => {
+    const store = testStore({
+      ...storeInitialState,
+      pairings: availableParticipants
+    })
+    await store.dispatch(autoAssignPairs())
+    return selectPairingGroups(store.getState())
+  }
+
+  const groupExists = (pairingGroups, studentId, coachId) => {
+    return pairingGroups.some(group =>
+      group.students.some(student => student.id === studentId)
+        && group.coaches.some(coach => coach.id === coachId)
+    )
+  }
 
   it('does assign a pair if there is a match in language', async () => {
-    const dispatch = jest.fn()
-    const getState = () => ({
-      ...storeInitialState,
-      pairings: {
-        students: [{id: 1, name: 'A', languages: ['JS'], group: 0}],
-        coaches: [{id: 2, name: 'B', languages: ['JS'], group: 0}]
-      }
+    const pairingGroups = await runAutoPairingsFor({
+      students: [{id: 1, name: 'A', languages: ['JS'], group: 0}],
+      coaches: [{id: 2, name: 'B', languages: ['JS'], group: 0}]
     })
-
-    await autoAssignPairs()(dispatch, getState)
-
-    expect(dispatch).toHaveBeenCalledWith(moveStudentToGroup({studentId: 1, groupId: 1}))
-    expect(dispatch).toHaveBeenCalledWith(moveCoachToGroup({coachId: 2, groupId: 1}))
+    expect(groupExists(pairingGroups, 1, 2)).toBeTruthy()
   })
 
   it('does not assign a pair if there are no matches in languages', async () => {
-    const dispatch = jest.fn()
-    const getState = () => ({
-      ...storeInitialState,
-      pairings: {
-        students: [{id: 1, name: 'A', languages: ['Python'], group: 0}],
-        coaches: [{id: 2, name: 'B', languages: ['JS'], group: 0}]
-      }
+    const pairingGroups = await runAutoPairingsFor({
+      students: [{id: 1, name: 'A', languages: ['Python'], group: 0}],
+      coaches: [{id: 2, name: 'B', languages: ['JS'], group: 0}]
     })
-
-    await autoAssignPairs()(dispatch, getState)
-
-    expect(dispatch).not.toHaveBeenCalled()
+    expect(groupExists(pairingGroups, 1, 2)).toBeFalsy()
   })
 
-  // it('does assign different pairs for different matches', async () => {
-  //   const dispatch = jest.fn()
-  //   const getState = () => ({
-  //     ...storeInitialState,
-  //     pairings: {
-  //       students: [
-  //         {id: 10, name: 'A', languages: ['JS'], group: 0},
-  //         {id: 11, name: 'B', languages: ['Python'], group: 0},
-  //       ],
-  //       coaches: [
-  //         {id: 20, name: 'X', languages: ['JS'], group: 0},
-  //         {id: 21, name: 'Y', languages: ['Python'], group: 0}
-  //       ]
-  //     }
-  //   })
-  //
-  //   await autoAssignPairs()(dispatch, getState)
-  //
-  //   expect(dispatch).toBeCalledWith(
-  //     moveStudentToGroup({studentId: 10, groupId: 1}),
-  //     moveCoachToGroup({coachId: 20, groupId: 1}),
-  //     moveStudentToGroup({studentId: 11, groupId: 2}),
-  //     moveCoachToGroup({coachId: 21, groupId: 2})
-  //   )
-  // })
+  it('does assign different pairs for different matches', async () => {
+    const pairingGroups = await runAutoPairingsFor({
+      students: [
+        {id: 10, name: 'A', languages: ['JS'], group: 0},
+        {id: 11, name: 'B', languages: ['Python'], group: 0},
+      ],
+      coaches: [
+        {id: 20, name: 'X', languages: ['JS'], group: 0},
+        {id: 21, name: 'Y', languages: ['Python'], group: 0}
+      ]
+    })
+
+    expect(groupExists(pairingGroups, 10, 20)).toBeTruthy()
+    expect(groupExists(pairingGroups, 11, 21)).toBeTruthy()
+  })
 
 })
