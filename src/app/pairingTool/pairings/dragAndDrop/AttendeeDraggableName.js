39e31c0fc0dd2c3be64392e1be149a3f25a1a5aa
Salva Juan <saljuama@gmail.com>
2022-11-11 12:13:00 +0100

    restore button

--- src/app/pairingTool/pairings/dragAndDrop/AttendeeDraggableName.js
+++ src/app/pairingTool/pairings/dragAndDrop/AttendeeDraggableName.js
@@ -3,38 +3,20 @@ import {useDrag} from 'react-dnd'
 import OpenWithIcon from '@mui/icons-material/OpenWith'
 import Button from '@mui/material/Button'
 
-const draggableButton = (color) => ({
-  cursor: 'move',
-  margin: '10px',
-  '& button': {
-    backgroundColor: 'whitesmoke',
-    '&:hover' : {backgroundColor: 'whitesmoke'}
-  },
-  '& button.IsDragging': {
-    backgroundColor: color,
-    '&:hover' : {backgroundColor: color}
-  }
-})
-
 export const AttendeeDraggableName = ({attendee, type}) => {
-  const [{isDragging}, drag] = useDrag(() => ({
+  const [collected, drag] = useDrag(() => ({
     type,
-    item: { id: attendee.id },
-    collect: monitor => ({
-      isDragging: !!monitor.isDragging()
-    })
+    item: { id: attendee.id }
   }), [attendee, type])
   return (
-    <div ref={drag} style={draggableButton(type === 'Student' ? 'lightpink' : 'lightsalmon')}>
-      {/*<Button*/}
-      {/*  className={isDragging ? 'IsDragging': ''}*/}
-      {/*  variant='contained'*/}
-      {/*  color='default'*/}
-      {/*  endIcon={<OpenWithIcon/>}*/}
-      {/*>*/}
-      {/*  {attendee.name}*/}
-      {/*</Button>*/}
-      {attendee.name} <OpenWithIcon/>
+    <div ref={drag} style={{cursor: 'move', margin: '10px'}}>
+      <Button
+        style={{backgroundColor: 'whitesmoke', '&:hover' : {backgroundColor: 'whitesmoke'}}}
+        variant='outlined'
+        endIcon={<OpenWithIcon/>}
+      >
+        {attendee.name}
+      </Button>
     </div>
   )
 }
