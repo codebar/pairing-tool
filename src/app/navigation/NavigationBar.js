8c778ed6ccdb574759888d50d419abaee82f2cd5
Salva Juan <saljuama@gmail.com>
2022-11-15 02:00:21 +0100

    add user manual link to navbar and update to styled components

--- src/app/navigation/NavigationBar.js
+++ src/app/navigation/NavigationBar.js
@@ -1,11 +1,10 @@
-/** @jsxImportSource @emotion/react */
-import {css} from '@emotion/react'
 import React from 'react'
+import styled from '@emotion/styled'
 import ContactSupportIcon from '@mui/icons-material/ContactSupport'
+import MenuBookIcon from '@mui/icons-material/MenuBook'
 import smallLogo from './logo200.png'
-import {FeedbackLink} from './FeedbackLink'
 
-const style = css`
+const NavBar = styled.div`
   padding: 5px 20px;
   display: flex;
   flex-flow: row nowrap;
@@ -15,28 +14,60 @@ const style = css`
   color: white;
   border-bottom: 1px solid grey;
   box-shadow: 2px 2px 2px 2px lightgrey;
+`
+const LeftNav = styled.div`
   img {
     width: 50px;
     height: 50px;
   }
+`
+const MiddleNav = styled.div`
   span {
     font-size: 24px;
     font-weight: bold;
     font-family: Roboto, serif;
   }
 `
+const RightNav = styled.div`
+  svg {
+    font-size: 40px;
+    color: white;
+    padding-right: 10px;
+  }
+`
+
+const UserManualLink = (props) =>
+  <a
+    href='https://github.com/codebar/pairing-tool/blob/main/doc/UserManual.md'
+    target='_blank'
+    rel='noreferrer'
+  >
+    {props.children}
+  </a>
+
+const FeedbackLink = (props) =>
+  <a
+    href='https://github.com/codebar/pairing-tool/issues/new/choose'
+    target='_blank'
+    rel='noreferrer'
+  >
+    {props.children}
+  </a>
 
 export const NavigationBar = () =>
-  <div css={style}>
-    <div>
+  <NavBar>
+    <LeftNav>
       <img alt='Codebar' src={smallLogo} />
-    </div>
-    <div>
+    </LeftNav>
+    <MiddleNav>
       <span>Pairing Tool</span>
-    </div>
-    <div>
+    </MiddleNav>
+    <RightNav>
+      <UserManualLink>
+        <MenuBookIcon/>
+      </UserManualLink>
       <FeedbackLink>
-        <ContactSupportIcon css={{fontSize: 40, color: '#fff4e5'}} />
+        <ContactSupportIcon/>
       </FeedbackLink>
-    </div>
-  </div>
+    </RightNav>
+  </NavBar>
